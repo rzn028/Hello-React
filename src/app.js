29243c3066367800bb2d232d649ed1148f208e6d
rajan singh <rajan.singh1@myoperator.co>
2020-03-27 16:36:02 +0530

    Hello React

---
+++ src/app.js
@@ -0,0 +1,87 @@
+var rootDiv = document.getElementById('app');
+
+class MyApp extends React.Component {
+
+    state = {
+        response = null
+    }
+
+    setResponse = (_resp) => {
+        this.setState({
+        response: _resp
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Form
+                    setResponse= {this.setResponse}
+                />
+                <ResultDiv
+                    response={this.state.response}  
+                />
+            </div>
+        );
+    }
+}
+
+
+class Form extends React.Component {
+
+    sendRequest(e) {
+        e.preventDefault();
+
+        let url = e.target.elements.url.value;
+        let method = e.target.elements.method.value;
+        let body = e.target.elements.body.value;
+
+
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        if(method =='POST' && body)
+            options['body'] = body;
+
+        fetch(url, options)
+        .then(res => res.json())
+        .then(res => {
+            this.props.setResponse(res);
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit ={this.sendRequest}>
+                <select name="method" required>
+                    <option value="GET">GET</option>
+                    <option value="POST">POST</option>
+                </select>
+                <input type = "text" name="url" required/>
+                <textarea name="body" rows="4" cols="40">
+
+                </textarea>
+                <button>Submit</button>
+            </form>
+        );
+    }
+
+}
+
+class ResultDiv extends React.Component {
+
+    render() {
+        return (
+            <div>
+                something here
+                {this.props.response}
+            </div>
+        )   
+    }
+}
+
+ReactDOM.render(<MyApp/>, rootDiv);
